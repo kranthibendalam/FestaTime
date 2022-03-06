fe756658480f6fba3cf2d159a8353a525f84e544
kranthi.bendalam <kranthiuni@outlook.com>
2022-03-06 22:23:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/festatime/SignUp.kt
@@ -0,0 +1,15 @@
+package com.example.festatime
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SignUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+    }
+}
